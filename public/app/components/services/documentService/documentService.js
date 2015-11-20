739cd327215672238b37829a6a356063c8e912ff
Thomas <Thomas@Subnotebook>
2015-11-20 19:25:43 +0100

    SOme UI improvements for start

--- public/app/components/services/documentService/documentService.js
+++ public/app/components/services/documentService/documentService.js
@@ -10,6 +10,7 @@
         module.getAllDocuments = function() {
 
             var items = [];
+            items.push({id: 1, name: 'Notiz', text: 'Klein'});
             items.push({id: 1, name: 'Notiz', text: '- Item 1\n- Item 2\n- Item 3\n- Item 4\n- Item 5\n- Item 6\n- Item 7\n- Item 8\n- Item 9\n- Item 10'});
             items.push({id: 2, name: 'Notiz', text: '#Lorem\n ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
             items.push({id: 3, name: 'Notiz', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
