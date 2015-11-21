fbc999ece8a35705caea9448c2baaa20e45db8bd
Thomas <Thomas@Subnotebook>
2015-11-21 17:48:31 +0100

    added sample of possible workaround for hotkey issue

--- public/app/document/document.js
+++ public/app/document/document.js
@@ -28,5 +28,25 @@
         $scope.edit = function() {
             $location.path('/document/' + $scope.document.id + '/edit');
         };
+
+        //possible (ugly) workaround if bug in angular hotkey isn't fixed
+        // $document.bind('keydown', function(e) {
+        //     var preventDefault = false;
+        //
+        //     if (e.keyCode === 'E'.charCodeAt(0) && e.ctrlKey && !e.shiftKey && !e.altKey) {
+        //         //CTRL + E -> edit
+        //         preventDefault = true;
+        //         $scope.edit();
+        //         $scope.$apply();
+        //     } else if (e.keyCode === 'S'.charCodeAt(0) && e.ctrlKey && !e.shiftKey && !e.altKey) {
+        //         //prevent showing browser 'save as' dialog for CTRL + S lovers...
+        //         preventDefault = true;
+        //     }
+        //
+        //     if (preventDefault) {
+        //         e.preventDefault();
+        //         return false;
+        //     }
+        // });
     }
 }());
