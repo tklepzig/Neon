65df9dc69d06fd619731ba394cf76108a530a912
Thomas <Thomas@DevBook>
2016-03-21 19:03:45 +0100

    added key navigation

--- public/app/document/edit/edit.js
+++ public/app/document/edit/edit.js
@@ -17,12 +17,29 @@
     }
 
     function EditController($scope, $location, $routeParams, documentService) {
-        $scope.focusDocument = false;
+        $scope.focusText = false;
+        $scope.focusName = false;
 
         documentService.getDocument($routeParams.id).then(function(document) {
             $scope.document = document;
-            $scope.focusDocument = true;
+            $scope.focusText = true;
         });
+
+        $scope.textKeyDown = function(e) {
+            //F2 or up arrow at position 0
+            if (e.keyCode === 113 || (e.keyCode === 38 && e.path.length > 0 && e.path[0].selectionStart === e.path[0].selectionEnd && e.path[0].selectionStart === 0)) {
+                $scope.focusText = false;
+                $scope.focusName = true;
+            }
+        };
+
+        $scope.nameKeyDown = function(e) {
+            //down arrow or enter
+            if (e.keyCode === 40 || e.keyCode === 13) {
+                $scope.focusText = true;
+                $scope.focusName = false;
+            }
+        };
 
         $scope.update = function() {
             documentService.updateDocument($scope.document);
