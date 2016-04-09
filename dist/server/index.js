524a151be234fce5055fa356446fd5eded179737
Thomas <Thomas@DevBook>
2016-04-09 18:30:58 +0200

    added colorized console output

--- dist/server/index.js
+++ dist/server/index.js
@@ -28,6 +28,7 @@ var app = express();
 var http = require('http').Server(app);
 var socketIo = require('socket.io')(http);
 var nconf = require('nconf');
+require('colors');
 
 var config = require('./config.json')[process.env.NODE_ENV || 'production'];
 nconf.file(path.resolve(__dirname + '/secrets.json')).env();
@@ -45,9 +46,15 @@ var dataService = require('./dataService.js')({
     password: secrets.repoPassword
 });
 var port = process.env.PORT || config.port;
-console.log('Configuration: ' + (process.env.NODE_ENV || 'production'));
+
+if (process.env.NODE_ENV === 'development') {
+    console.log('Configuration: ' + 'development'.bold.green);
+} else {
+    console.log('Configuration: ' + (process.env.NODE_ENV || 'production').bold.red);
+}
+
 console.log('using ' + config.publicFilePath + ' to serve public files');
-console.log('Push is ' + (config.isPushAllowed ? 'enabled' : 'disabled'));
+console.log('Push is ' + (config.isPushAllowed ? 'enabled'.bold.red : 'disabled'.bold.green));
 
 app.use(express.static(path.resolve(__dirname + config.publicFilePath)));
 app.get('/*', function(req, res) {
@@ -91,10 +98,15 @@ socketIo.on('connection', function(socket) {
         callback(document);
     });
 
-    // socket.on('removeDocument', function(id) {
-    //     dataService.removeDocument(id);
-    //     socket.broadcast.emit('documentRemoved', id);
-    // });
+    socket.on('removeGroup', function(id) {
+        dataService.removeGroup(id);
+        socket.broadcast.emit('groupRemoved', id);
+    });
+
+    socket.on('removeDocument', function(id) {
+        dataService.removeDocument(id);
+        socket.broadcast.emit('documentRemoved', id);
+    });
 
     socket.on('getGroup', function(id, callback) {
         var group = dataService.getGroup(id);
